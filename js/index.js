33773d85aee1f5aebffb0217afc55dde7c1ca1af
João Garrido <joao.melo.garrido@hotmail.com>
2021-03-23 20:05:21 +0000

    some better css and form bettter putnot finished tho

--- js/index.js
+++ js/index.js
@@ -153,8 +153,8 @@ function submitEmail(){
     
     async function handleSubmit(event) {
       event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
+      let status = document.getElementById("my-form-status");
+      let data = new FormData(event.target);
       fetch(event.target.action, {
         method: form.method,
         body: data,
