a36a42de3b1f6df566ce403da8138d66024ef945
João Garrido <joao.melo.garrido@hotmail.com>
2021-03-24 16:57:16 +0000

    new image

--- js/index.js
+++ js/index.js
@@ -14,92 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     formVisibility();
-    
 
-  /*  // Form submit validation
-    const formName = document.getElementById("formName");
-    const formEmail = document.getElementById("formEmail");
-    const formText = document.getElementById("formText");
-    const form = document.getElementById("form");
-
-    formName.addEventListener('input', function () {
-        let alert1 = document.getElementById('alert-1');
-        let error = this.value === '';
-        showError(alert1, error);
-    });
-    formEmail.addEventListener('input', function () {
-        let alert2 = document.getElementById('alert-2');
-        let error = this.value === '';
-        showError(alert2, error);
-    });
-    formText.addEventListener("input", function () {
-        let alert3 = document.getElementById('alert-3');
-        let error = this.value === '';
-        showError(alert3, error);
-    });
-
-    form.addEventListener('submit', function (e) {
-        let input = document.querySelectorAll('input');
-        let textArea = document.querySelectorAll('textarea');
-        let checkedEmail = checkEmail(formEmail.value);
-        if (formName.value === '' || formEmail.value === '' || formText.value === '' || checkedEmail === false) {
-            e.preventDefault();
-            for (i = 0; i < input.length; i++) {
-                if (input[i].value === '') {
-                    input[i].nextElementSibling.style.opacity = '1';
-                    //input[i].style.backgroundColor = "red";
-                    // TODO : PERFORM BETTER VALIDATION
-                }
-            }
-            for (i = 0; i < textArea.length; i++) {
-                if (textArea[i].value === '') {
-                    textArea[i].nextElementSibling.style.opacity = '1';
-                }
-            }
-        } else {
-            e.preventDefault();
-            const button = document.querySelector('.submit-button');
-            button.classList.add('state-1', 'animated');
-
-            const formData = new FormData(this);
-            fetch("php/mail.php", {
-                method: 'post',
-                body: formData
-            }).then((response) => {
-                return response.text();
-            }).then((text) => {
-                if (text === "sent") {
-                    button.classList.add('state-2');
-                    setTimeout(function () {
-                        button.classList.remove('state-1', 'state-2', 'animated');
-                        form.reset();
-                    }, 2000);
-                } else {
-                    button.classList.add('state-error');
-                    setTimeout(function () {
-                        button.classList.remove('state-1', 'state-error', 'animated');
-                    }, 2000);
-                }
-            }).catch((error) => {
-                console.log(error);
-                button.classList.add('state-error');
-                setTimeout(function () {
-                    button.classList.remove('state-1', 'state-error', 'animated');
-                }, 2000);
-            });
-        }
+    const actionBtn = document.querySelector(".action-btn");
+    actionBtn.addEventListener('click', () => {
+        actionBtn.classList.add("loading");
+        actionBtn.disabled = true;
+        setTimeout(() => actionBtn.classList.remove("loading"), 3000);
     });
 });
-*/
-});
 
-/*function showError(element, error) {
-    if (error === true) {
-        element.style.opacity = '1';
-    } else {
-        element.style.opacity = '0';
-    }
-};
+/*
 function checkEmail(value) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
         return (true)
@@ -130,7 +54,7 @@ function createTags() {
     });
 }
 
-function formVisibility(){
+function formVisibility() {
     //SHOW/HIDE form
     const letsBuild = document.getElementById("letsBuild");
     const modal = document.querySelector(".modal");
@@ -150,9 +74,10 @@ function formVisibility(){
 
 function submitEmail(){
     let form = document.getElementById("my-form");
-    
+
     async function handleSubmit(event) {
       event.preventDefault();
+        /*
       let status = document.getElementById("my-form-status");
       let data = new FormData(event.target);
       fetch(event.target.action, {
@@ -167,6 +92,7 @@ function submitEmail(){
       }).catch(error => {
         status.innerHTML = "Oops! There was a problem submitting your form"
       });
+      */
     }
     form.addEventListener("submit", handleSubmit)
 }
