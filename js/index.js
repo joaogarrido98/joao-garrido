031a89c3c83e63d6d2dd5cfc83589b31cd35c2cb
João Garrido <joao.melo.garrido@hotmail.com>
2021-04-04 22:48:58 +0100

    more pleasing

--- js/index.js
+++ js/index.js
@@ -40,20 +40,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     getProjects();
 
-    const state = {};
-    const carouselList = document.querySelector('.carousel__list');
-    const carouselItems = document.querySelectorAll('.carousel__item');
-    const elems = Array.from(carouselItems);
+    document.querySelector("#right").addEventListener('click', () => {
+        document.getElementById('row').scrollLeft += 500;
+    });
 
-    carouselList.addEventListener('click', function (event) {
-        var newActive = event.target;
-        var isItem = newActive.closest('.carousel__item');
+    document.querySelector("#left").addEventListener('click', () => {
+        document.getElementById('row').scrollLeft -= 500;
+    });
 
-        if (!isItem || newActive.classList.contains('carousel__item_active')) {
-            return;
-        };
+    document.querySelector("#all").addEventListener('click', (evt) => {
+        tabManage(evt);
+        getProjects();
+    });
 
-        update(newActive, elems, state);
+    document.querySelector("#web").addEventListener('click', (evt) => {
+        tabManage(evt);
+        getProjects();
+    });
+    document.querySelector("#android").addEventListener('click', (evt) => {
+        tabManage(evt);
+        getProjects();
     });
 });
 
@@ -241,51 +247,36 @@ function checkPosition() {
     }
 }
 
-function update(newActive, elems, state) {
-    const newActivePos = newActive.dataset.pos;
-    const current = elems.find((elem) => elem.dataset.pos == 0);
-    const prev = elems.find((elem) => elem.dataset.pos == -1);
-    const next = elems.find((elem) => elem.dataset.pos == 1);
-    const first = elems.find((elem) => elem.dataset.pos == -2);
-    const last = elems.find((elem) => elem.dataset.pos == 2);
-
-    current.classList.remove('carousel__item_active');
-
-    [current, prev, next, first, last].forEach(item => {
-        var itemPos = item.dataset.pos;
-
-        item.dataset.pos = getPos(itemPos, newActivePos)
-    });
-};
-
-function getPos(current, active) {
-    const diff = current - active;
-
-    if (Math.abs(current - active) > 2) {
-        return -current
-    }
-
-    return diff;
-}
-
 function getProjects() {
-    fetch('https://joaogarrido98.github.io/joao-garrido/json/projects.json', {
+    fetch('../json/projects.json', {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-    }
-    )
+    })
         .then(response => {
             return response.json();
         })
         .then(function (data) {
             let projects = data["projects"];
-            let length = Object.keys(projects).length;
-            let i;
-            for(i in projects){
-                console.log(projects[i]);
-                createCard(projects[i]);
+            let i, title;
+            let carousel = document.querySelector(".carousel-inner");
+            for (i in projects) {
+                if (projects.hasOwnProperty(i)) {
+                    title = i;
+                }
+                let card = `<div class="tile__media">
+                  <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-1.jpg" alt="" />
+                </div>
+                <div class="tile__details">
+                  <div class="tile__title">
+                    ${title}
+                  </div>
+                </div>`;
+                const ele = document.createElement('div');
+                ele.innerHTML = card;
+                ele.className = "tile";
+                carousel.appendChild(ele);
             }
         })
         .catch(error => {
@@ -293,6 +284,9 @@ function getProjects() {
         })
 }
 
-function createCard(element){
-
+function tabManage(evt){  
+    Array.from(document.querySelectorAll('.tab')).forEach(function(el) { 
+        el.classList.remove('chosen');
+    });
+    evt.target.classList.add("chosen");
 }
